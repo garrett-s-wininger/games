a1258b42c77381cd7dfaf00b32d9252e93530da0
Garrett Wininger <garrett.s.wininger@outlook.com>
2024-01-13 15:45:02 -0500

    Initial ground tileset, arena layout for character testing

--- Jams/PSGJ14/scripts/level.gd
+++ Jams/PSGJ14/scripts/level.gd
@@ -1,10 +1,5 @@
 extends Node2D
 
-func _process(_delta):
-	# TODO: Remove this after all pause functionality is working, placeholder UI
-	var label = $CanvasLayer/MarginContainer/VBoxContainer/CounterLabel
-	label.text = str(int(label.text) + 1)
-
 func _input(event):
 	# On a cancel action, take the user to the pause menu where they should
 	# be able to return to the main menu or exit out of the application altogether
