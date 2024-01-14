f42ab3ae08dfc5df14f4483b254f7305e4c5c9bd
Garrett Wininger <garrett.s.wininger@outlook.com>
2024-01-13 22:38:11 -0500

    Collision detection w/ tilemap, demo map rework

--- Jams/PSGJ14/scripts/player.gd
+++ Jams/PSGJ14/scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	velocity = Input.get_vector("left", "right", "up", "down") * SPEED
 	move_and_slide()
 	look_at(get_global_mouse_position())
